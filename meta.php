d81da9c6e913a0e68e1308baee47281822851eae
ctaill <c.taillandierpro@gmail.com>
2017-04-06 14:00:33 +0200

    ajout des images du catalogue et de la partie meta du code html

---
+++ meta.php
@@ -0,0 +1,21 @@
+<?php
+function meta(){
+echo "<!DOCTYPE html>";
+echo "<html>";
+echo "<head>";
+echo  "<meta charset=\"utf-8\">";
+echo  "<meta content=\"IE=edge,chrome=1\" http-equiv=\"X-UA-Compatible\">";
+echo  "<!-- Controle de la mise en page et du zoom pour les autres appareils -->";
+echo  "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">";
+echo  "<meta name=\"author\" content=\"TAILLANDIER Camille/Baptiste Aupied\">";
+echo  "<meta name=\"description\" content=\"Visionneuse\">";
+
+      
+echo  "<link rel='stylesheet' href='style.css'>";
+echo  "<title>Visionneuse web pour bornes</title>";
+echo  "<link href=\"https://fonts.googleapis.com/css?family=Revalia\" rel=\"stylesheet\">";
+echo  "<link href=\"https://fonts.googleapis.com/css?family=Arima+Madurai\" rel=\"stylesheet\">";
+}
+
+
+?>
